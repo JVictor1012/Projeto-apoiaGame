b28af3b1ccc10493d9b9a3c4dc3d27bc08d33612
Jvictor Nunes <jvictornunes1012@gmail.com>
2023-01-12 12:57:54 -0300

    adicionando rotas

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Home from './Home';
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Home />
+  <React.StrictMode>  
+      <BrowserRouter>
+        <Routes>
+          <Route path="/home" element={<Home />} exact />
+        </Routes>
+      </BrowserRouter>
   </React.StrictMode>
 );
